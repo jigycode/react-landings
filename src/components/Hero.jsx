84c92be9c5713ef22d678c7865f19828219ed363
jigycode <jigyasha040106@gmail.com>
2024-12-16 15:16:05 +0530

    navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#27a0f0] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Fast, flexible financing for
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
